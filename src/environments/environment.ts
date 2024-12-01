39655b398ab0e88606347e227d32bf44fd2377aa
bapro-admin <carlosmandujano.v@gmail.com>
2024-12-01 14:10:52 -0600

    build 3.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,9 @@
 export const environment = {
+  production: true, 
+  API_URL: 'https://bapro.info/api/'
+};
+/* export const environment = {
     production: false, 
     API_URL: 'http://localhost:8000/api'
     //http://localhost:4200/#/login
-  };
+  }; */
